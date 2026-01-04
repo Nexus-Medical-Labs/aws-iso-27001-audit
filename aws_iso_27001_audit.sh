70c066e864e44fca8cecb1fc21097e26a126dbc8
Josiah Altschuler <josiah.altschuler@gmail.com>
2026-01-03 19:30:37 -0600

    Output start of audit and timestamp

--- aws_iso_27001_audit.sh
+++ aws_iso_27001_audit.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
+echo "========================================="
+echo " START AWS ISO 27001 AUDIT"
+echo "========================================="
+
+echo
+echo "This report generated at: $(date -u +%Y-%m-%dT%H:%M:%SZ)"
