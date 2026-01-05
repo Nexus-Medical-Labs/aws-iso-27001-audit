f2149eedf494007dc973029a5f1ab920cc45cc58
Josiah Altschuler <josiah.altschuler@gmail.com>
2026-01-05 14:58:51 -0600

    Add dependency checks for aws and jq

--- aws_iso_27001_audit.sh
+++ aws_iso_27001_audit.sh
@@ -7,3 +7,9 @@ echo "========================================="
 
 echo
 echo "This report generated at: $(date -u +%Y-%m-%dT%H:%M:%SZ)"
+
+# This script requires that the following are installed:
+# - aws CLI (configured to your AWS account)
+# - jq
+command -v aws >/dev/null 2>&1 || { echo "aws CLI required"; exit 1; }
+command -v jq >/dev/null 2>&1 || { echo "jq required"; exit 1; }
